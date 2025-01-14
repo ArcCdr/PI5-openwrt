dec016a0d5cc48e9bab9a1b668014a3aef252d1e
ArcCdr <vincentrycx@hotmail.com>
2025-01-14 10:15:57 +0100

    Extracted part creation to separate script

--- 85-customize.sh
+++ 85-customize.sh
@@ -1,18 +1,6 @@
 # Check prerequisites
 if [ ! -e /etc/arcadian-customized ]; then
 
-    parted /dev/mmcblk0 mkpart primary ext4 5001MB 100%
-    mkfs.ext4 /dev/mmcblk0p3
-    mkdir /mnt/data
-    uci add fstab mount
-    uci set fstab.@mount[-1].target='/mnt/data'
-    uci set fstab.@mount[-1].device='/dev/mmcblk0p3'
-    uci set fstab.@mount[-1].fstype='ext4'
-    uci set fstab.@mount[-1].enabled='1'
-    uci commit fstab
-    /etc/init.d/fstab enable
-    /etc/init.d/fstab start
-
     # Paths to files
     BANNER_FILE="/etc/banner"
     LOGO_FILE="mylogo.txt"
